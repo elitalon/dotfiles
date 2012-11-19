8f2bd1cf5a4275097879e57df5d92183c2933a65
Eliezer Talon <elitalon@gmail.com>
2012-11-19 10:57:49 +0000

    Upgrade Janus under user request

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,20 +9,19 @@ function doIt() {
   if [ -d ~/.vim ]; then
     EXCLUDE_JANUS='--exclude ".vim"'
   fi
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" ${EXCLUDE_JANUS} -av . ~
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" ${EXCLUDE_JANUS} -av . ~
+  cd ~/.vim && rake && cd ${OLDPWD}
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+  doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt
+  fi
 fi
 unset doIt
 
 source ~/.bash_profile
-
-cd ~/.vim && rake && cd ${OLDPWD}
