beae8480a3cb6e58bfc04fdda8b8102babdf1b73
Eliezer Talón <elitalon@fastmail.com>
2025-07-20 18:30:39 +0200

    Extract Bash script template to a source file

--- scripts/mkscript.sh
+++ scripts/mkscript.sh
@@ -4,6 +4,9 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+###
+# Log functions
+###
 function fail() {
     local message=${1:-"Error"}
     local code=${2:-1}
@@ -21,96 +24,34 @@ function warn() {
     echo -e "\033[91m${message}\e[0m"
 }
 
-function notice() {
-    local message=${1:-}
-    echo -e "\033[93m${message}\e[0m"
-}
-
 function info() {
     local message=${1:-}
     echo -e "${message}"
 }
 
+###
+# Creates a script out of the template
+###
 function make_template() {
+    local template_script
+    template_script=template.sh
+    [[ -f "${template_script}" ]] \
+        || fail "Template script ${template_script} does not exist" 2
+
     local filename=${1:-}
-    [[ -z "${filename}" ]] && fail "missing filename" 2
-    [[ -e "${filename}" ]] && fail "${filename} already exists" 3
+    [[ -z "${filename}" ]] && fail "Missing filename" 3
+    [[ -e "${filename}" ]] && fail "Script ${filename} already exists" 3
 
     info "Creating ${filename}"
-    touch "${filename}"
-
-    cat << 'END_OF_TEMPLATE' > "${filename}"
-#!/usr/bin/env bash
-
-# -u, treat unset variables and parameters as an error"
-set -o nounset
-
-# -e, exit immediately if a pipeline returns a non-zero status
-set -o errexit
-
-# The return value of a pipeline is the value of the last (rightmost) command
-# to exit with a non-zero status, or zero if all commands in the pipeline exit
-# successfully
-set -o pipefail
-
-
-SCRIPT_DIRECTORY=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-readonly SCRIPT_DIRECTORY
-
-function fail() {
-    local message=${1:-"Error"}
-    local code=${2:-1}
-
-    if [[ $code -eq 0 ]]; then
-        code=1
-    fi
-
-    warn "${message} (${code})"
-    exit "$code"
-}
-
-function warn() {
-    local message=${1:-}
-    echo -e "\033[91m${message}\e[0m"
-}
-
-function notice() {
-    local message=${1:-}
-    echo -e "\033[93m${message}\e[0m"
-}
-
-function info() {
-    local message=${1:-}
-    echo -e "${message}"
-}
-
-#############################
-# Write your functions here #
-#############################
-function hello_world() {
-    info "Hello, world!"
-}
-
-function main() {
-    ##############################
-    # Invoke your functions here #
-    ##############################
-    hello_world
-}
-
-main "$@"
-END_OF_TEMPLATE
+    cat "${template_script}" > "${filename}"
 
     info "Making ${filename} executable"
     chmod 0755 "${filename}"
-
-    info "Executing ${filename}"
-    # shellcheck source=/dev/null
-    . "${filename}"
-
-    info "Done!"
 }
 
+###
+# Main function
+###
 function main() {
     make_template "${1:-}"
 }
---
+++ scripts/template.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+# Enable shell behaviours if script is not being sourced
+# Via: https://stackoverflow.com/a/28776166/8787985
+if ! (return 0 2> /dev/null); then
+    set -o nounset  # Treat unset variables and parameters as an error
+    set -o errexit  # Exit if a pipeline returns a non-zero status
+    set -o pipefail # Use last non-zero exit code in a pipeline
+fi
+
+###
+# Constants
+###
+SCRIPT_DIRECTORY=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+readonly SCRIPT_DIRECTORY
+
+###
+# Log functions
+###
+function fail() {
+    local message=${1:-"Error"}
+    local code=${2:-1}
+
+    if [[ $code -eq 0 ]]; then
+        code=1
+    fi
+
+    warn "${message} (${code})"
+    exit "$code"
+}
+
+function warn() {
+    local message=${1:-}
+    echo -e "\033[91m${message}\e[0m"
+}
+
+function notice() {
+    local message=${1:-}
+    echo -e "\033[93m${message}\e[0m"
+}
+
+function info() {
+    local message=${1:-}
+    echo -e "${message}"
+}
+
+###
+# Whether a program exists in the search path
+###
+function program_exists() {
+    if [[ $# -lt 1 ]]; then
+        fail "Missing binary name argument in program_exists" 2
+    else
+        command -v "$1" 1>/dev/null 2>&1
+    fi
+}
+
+###
+# Write your functions here
+###
+function hello_world() {
+    info "Hello, world!"
+}
+
+###
+# Main function
+###
+function main() {
+    # Invoke your functions here
+    hello_world
+}
+
+# Invoke main with args if script is not being sourced
+# Via: https://stackoverflow.com/a/28776166/8787985
+if ! (return 0 2> /dev/null); then
+    main "$@"
+fi
