4cab24366a5bf09668bb0bf72412d7d274efb34b
Eliezer Talón <elitalon@fastmail.com>
2026-01-05 09:48:33 +0100

    Add support for opening IntelliJ IDEA with different names

--- idea.sh
+++ idea.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-open -na "IntelliJ IDEA.app" --args "$@"
+open_intellij() {
+    path=${1:-}
+    shift
+
+    open -na "${path}" --args "$@" 2>/dev/null
+}
+
+main() {
+    open_intellij "IntelliJ IDEA.app" "$@" || open_intellij "IntelliJ IDEA CE.app" "$@"
+}
+
+main "$@"
