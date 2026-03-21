82f8524ae34c37f802ad95e79b9e72a3b32ff0a6
Eliezer Talón <elitalon@fastmail.com>
2026-03-21 12:31:40 +0100

    Add support to idea.sh to activate IntelliJ after opening

--- idea.sh
+++ idea.sh
@@ -1,14 +1,35 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-open_intellij() {
-    path=${1:-}
-    shift
+set -o nounset
 
-    open -na "${path}" --args "$@" 2>/dev/null
+function activate_intellij() {
+	local bundle_id=${1:-}
+	osascript -e "tell application id \"${bundle_id}\" to activate"
 }
 
-main() {
-    open_intellij "IntelliJ IDEA.app" "$@" || open_intellij "IntelliJ IDEA CE.app" "$@"
+function open_intellij() {
+	local bundle_id=${1:-}
+	shift
+
+	open -b "${bundle_id}" --args "$@"
+	# activate_intellij "${bundle_id}"
+}
+
+function find_bundle_id() {
+	local app_names=("IntelliJ IDEA" "IntelliJ IDEA CE")
+
+	local bundle_id
+	for app_name in "${app_names[@]}"; do
+		bundle_id=$(osascript -e "id of app \"${app_name}\"" 2>/dev/null)
+		[[ -n "${bundle_id}" ]] && echo "${bundle_id}" && return
+	done
+
+	echo "No bundle ID found" >&2
+	exit 1
+}
+
+function main() {
+	open_intellij "$(find_bundle_id)" "$@"
 }
 
 main "$@"
